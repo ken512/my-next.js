ad1f476c134e48d3794740e90c9a0e0a6204b401
porota <ken887265@gmail.com>
2024-07-28 13:47:33 +0900

    commit

--- src/_types/MicroCmsPost.ts
+++
@@ -1,9 +0,0 @@
-export interface MicroCmsPost {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: string;
-  categories: { id: string; name: string }[]; 
-  thumbnailUrl: { url: string; height: number; width: number };
-}
-
